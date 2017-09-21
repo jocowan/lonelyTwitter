a1ca445993285021f7ade8f167345b5452e2bd6c
jocowan <jcowan@ualberta.ca>
2017-09-20 18:29:28 -0600

    Add Tweet class and

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -43,5 +43,10 @@ public abstract class Tweet implements Tweetable{
     }
 
     public abstract Boolean isImportant();
+
+    @Override
+    public String toString(){
+        return date.toString() + " | " + message;
+    }
 }
 
